2ad09a69780f20b1a7b610a1952e6240253a007f
Matt Feenstra <matt.feenstra@move.com>
2013-12-05 13:10:46 -0800

    added files

---
+++ myprog.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+puts "hello world!\n"
+puts "and stuff\n"
+puts "enter something "
+thisvar = gets
+puts "you enterned " + thisvar
+
+
+
---
+++ prog1.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+class Greeter
+
+	def initialize(name = "World")
+		@name = name
+	end
+
+	def say_hi
+		puts "Hi #{@name}!"
+	end
+
+	def say_bye
+		puts "bye #{@name}!"
+	end
+end
+
+
